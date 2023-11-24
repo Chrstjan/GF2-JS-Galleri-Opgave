d0caf03c366ddeecfc5f759e9341387e6a8481a5
Christian <ps5christian@hotmail.com>
2023-11-24 12:05:11 +0100

    Made the gallery
    Need to make the modal image

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,1 +1,31 @@
 // Din cool code here
+const animalsArray = [
+  "Dog.jpg",
+  "elephant.jpg",
+  "great-white.jpg",
+  "koala.jpg",
+  "tiger.jpg",
+];
+
+const baseUrl = "./assets/img/";
+
+const app = document.getElementById("app");
+
+const gallery = document.createElement("article");
+gallery.classList.add("animals-gallery");
+app.appendChild(gallery);
+
+const createImageGallery = () => {
+  animalsArray.forEach((img) => {
+    const galleryFigure = document.createElement("figure");
+    galleryFigure.classList.add("gallery-figure");
+    const galleryImage = document.createElement("img");
+    const galleryImageSrc = baseUrl + img;
+    galleryImage.src = galleryImageSrc;
+
+    gallery.appendChild(galleryFigure);
+    galleryFigure.appendChild(galleryImage);
+  });
+};
+
+createImageGallery();
